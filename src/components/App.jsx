91fa5169b7edc417f2a4d59a3190e77fb83a0b70
benjamin <ebenjaminhd11@gmail.com>
2025-03-20 21:49:20 -0500

    Adding route to login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Signup from './signup/Signup'
+import Signin from './signin/Signin'
 import Home from './home/Home'
 
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Signup />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Signin />} />
           <Route path='/home' element={<Home />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/signin/Signin.jsx
@@ -0,0 +1,11 @@
+import Cover from '../cover/Cover'
+
+function Signin() {
+  return (
+    <main className="principal">
+        <Cover />
+    </main >
+  )
+}
+
+export default Signin
